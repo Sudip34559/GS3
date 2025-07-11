c0086ed3b55e1cf30bdd32f4662db46eff114d06
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-07-11 16:08:28 +0530

    implemented register,login and logout feature using  JWT and bcrypt with middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import User from '../models/user.model.js';
+
+export default async function (req, res, next) {
+  const token = req.cookies?.token;
+  if (!token) {
+    return res.status(401).json({ message: "Authentication required" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ email: decoded.email }).select("-password");
+    if (!user) return res.status(404).json({ message: "User not found" });
+    req.user = user;
+    next();
+  } catch (error) {
+    return res.status(401).json({ message: "Invalid or expired token" });
+  }
+}
