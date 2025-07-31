a10cd6915f56de5fbfd4426276b55a19a3bcfc7b
MdAfnanNaushad <mdafnannaushad@gmail.com>
2025-07-31 23:16:57 +0530

    Ready for production

--- src/index.js
+++ src/index.js
@@ -10,12 +10,25 @@ export const client = new Groq({
   apiKey: process.env.GROQ_API_KEY,
 });
 
+// connectDB()
+//   .then(() => {
+//     app.listen(process.env.PORT || 8000, () => {
+//       console.log(` listening on http://localhost:${process.env.PORT}`);
+//     });
+//   })
+//   .catch((err) => {
+//     console.log("mongodb connection failed: " + err);
+//   });
+
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
-      console.log(` listening on http://localhost:${process.env.PORT}`);
+    const PORT = process.env.PORT || 8000;
+    // Explicitly listen on 0.0.0.0 to be accessible by Render
+    app.listen(PORT, '0.0.0.0', () => {
+      console.log(`Server is running on port: ${PORT}`);
     });
   })
   .catch((err) => {
-    console.log("mongodb connection failed: " + err);
+    console.log("❌ MongoDB connection failed: ", err);
+    process.exit(1); 
   });
